511ae1afb7394e57850968ca5faeb53aa839db0c
Marian Zlatev <mzlatev91@gmail.com>
2018-03-18 17:33:33 +0200

    convert the application to a web app
    add keynote and powerpoint presentations

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Example of DI and Hexagonal Architecture
+
+
+#### The project attempts to ilustrate how a hexagonal architecture can be implemented.
+
+
+The project consists of 4 modules:
+- `core.professionals`
+- `gateway`
+- `persistence`
+- `web`
+
+The core functionality or the business logic is implemented in `core.professionals`.
+That module does not have a dependency to the the other 3 modules.  
+
+The rest of the modules represent details that are not essential to the business logic.
+Note that those details are <b>package private</b> and are visible only in the module where
+they are defined. In this way they remain hidden from the `core.professionals` module which 
+is what is essential in our application. 
+
+Those details are: 
+- `gateway` - a module that implements logic for contacting external to the system payment providers.
+- `persistence` - a module that implements specific tooling like storage mechanisms.
+- `web` - a module that represents a transportation mechanism or in this case http.
--- src/main/java/com/jprofessionals/di/App.java
+++ src/main/java/com/jprofessionals/di/App.java
@@ -1,50 +1,20 @@
 package com.jprofessionals.di;
 
-import com.jprofessionals.di.core.professionals.Payment;
-import com.jprofessionals.di.core.professionals.ProfessionalPayments;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.jprofessionals.di.core",
-//        "com.jprofessionals.di.web", // start without this module (just because I can)
+        "com.jprofessionals.di.web",
         "com.jprofessionals.di.gateway",
         "com.jprofessionals.di.persistence",
 })
 public class App {
 
   public static void main(String[] args) {
-
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-
-    ProfessionalPayments payments = context.getBean(ProfessionalPayments.class);
-
-    System.out.println("=====================================");
-
-    String paymentId = getPaymentId(args);
-
-    Payment payment = payments.find(paymentId);
-
-    printPayment(paymentId, payment);
-
-    System.out.println("=====================================");
-
-    context.close();
-  }
-
-  private static String getPaymentId(String[] args) {
-    return (args != null && args.length > 0) ? args[0] : "0";
-  }
-
-  private static void printPayment(String id, Payment payment) {
-    if (payment == null) {
-      System.out.println("Payment with id '" + id + "' was not found");
-      return;
-    }
-
-    System.out.println(payment);
+    SpringApplication.run(App.class, args);
   }
 
 }
